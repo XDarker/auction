df7dbd46ef9269e66e9ab0b5c8e156efabbc2115
XDarker <xiashengjianah@163.com>
2019-06-25 23:16:56 +0800

    product module

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { STRING_TYPE } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-product',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
+  private products: Array<Product>;
+
   constructor() { }
 
   ngOnInit() {
+
+    this.products = [
+      new Product(1, "第一个商品", 1.99, 3.5, "这是第一个商品，是我在学习Angular实战时创建的", ["电子产品", "硬件设备"]),
+      new Product(2, "第二个商品", 1.99, 2.5, "这是第一个商品，是我在学习Angular实战时创建的", ["图书"]),
+      new Product(3, "第三个商品", 1.99, 4.5, "这是第一个商品，是我在学习Angular实战时创建的", ["硬件设备"]),
+      new Product(4, "第四个商品", 1.99, 1.5, "这是第一个商品，是我在学习Angular实战时创建的", ["电子产品", "硬件设备"]),
+      new Product(5, "第五个商品", 1.99, 3.5, "这是第一个商品，是我在学习Angular实战时创建的", ["电子产品"]),
+      new Product(6, "第六个商品", 1.99, 2.5, "这是第一个商品，是我在学习Angular实战时创建的", ["服装"]),
+    ];
   }
 
+}
+
+export class Product {
+  constructor(
+    public id: number,
+    public title: string,
+    public price: number,
+    public rating: number,
+    public desc: string,
+    public categories: Array<string>
+  ) {
+  }
 }
