60c280335c9d659f9e993c8caf7abd46e2aa28ba
XDarker <xiashengjianah@163.com>
2019-06-25 22:09:57 +0800

    angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
+// 组件源数据装饰器
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'XDarker 元气满满的一天';
+}
+
